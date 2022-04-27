c2b0f05de4711d156fabb1b1176477287a1fae32
ethernal12 <korbar41@gmail.com>
2022-04-27 17:27:18 +0200

    Adding beforeAll tests and axios.

--- test/test1.js
+++ test/test1.js
@@ -1,14 +1,32 @@
 const assert = require("assert");
 const axios = require("axios").default;
 
-const instance = axios.create({
+const req = axios.create({
     baseURL: 'http://localhost:3000',
     timeout: 1000
-  });
+});
 
-describe("Test1", () => {
-    it("Should pass.", () => {
 
+
+describe("GET /appointments", () => {
+
+    before((done) => {
+        const self = this;
+        req.get("/appointments")
+            .then(function (res) {
+                self.res = res;
+                done();
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+
+
+
+    })
+    it("Should have right JSON schema.", () => {
+        console.log(this.res);
         assert(false, "This should be true statement.");
 
     })
